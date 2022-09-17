2b37d063e1a2c48d60fa0a23d632328ef338882f
Joycilane Carvalho <joycarvalho59@gmail.com>
2022-09-17 19:59:04 -0300

    NavBar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar"
+
+export default function App() {
+    return (
+        <div class="container">
+            <header>
+                <NavBar/>
+            </header>
+        </div>
+    )
+}
---
+++ src/NavBar.js
@@ -0,0 +1,31 @@
+export default function NavBar() {
+    return(
+    <div class="topo">
+        <div class="iconinsta">
+            <div class="iconedoinsta">
+                <ion-icon name="logo-instagram"></ion-icon>
+            </div>
+            <img src="img/project/logo.png" alt=""/>
+        </div>
+        <div class="box" >
+            <div >
+                <p>pesquisar</p>
+            </div>
+        </div>
+        <div class="icons2">
+            <div class="icones setatopo">
+                <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div class="icones direitatopo">
+                <ion-icon name="compass-outline"></ion-icon>
+            </div>
+            <div class="icones direitatopo">
+                <ion-icon name="heart-outline"></ion-icon>
+            </div>
+            <div class="icones direitatopo">
+                <ion-icon name="person-outline"></ion-icon>
+            </div>
+        </div>
+    </div>
+    )
+}
