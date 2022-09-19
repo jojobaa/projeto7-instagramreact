6b8eb9dde16d7fe1d93d4cd0034205182cc98a19
Joycilane Carvalho <joycarvalho59@gmail.com>
2022-09-18 23:13:25 -0300

    versão finalizada

--- src/Posts.js
+++ src/Posts.js
@@ -1,4 +1,29 @@
+import React from "react"
+
 function Post(props) {
+    const [color, setColor] = React.useState("")
+    const [icon, setIcon] = React.useState("heart-outline")
+    const [salvarPost, setSalvar] = React.useState("bookmark-outline")
+    const [like, setLike] = React.useState(0)
+
+    function likePost() {
+        setColor("deiLike md hydrated")
+        setIcon("heart")
+        setLike(like + 1)
+        if (icon === "heart") {
+            setColor("md hydrated")
+            setIcon("heart-outline")
+            setLike(like - 1)
+        }
+    }
+
+    function savePost(){
+        setSalvar("bookmark")
+        if(salvarPost === "bookmark"){
+            setSalvar("bookmark-outline")
+        }
+    }
+
     return (
         <div class="post">
             <div class="posts">
@@ -15,17 +40,17 @@ function Post(props) {
                 <div class="abaixodopost">
                     <div class="iconspost">
                         <div class="lefticons">
-                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon name={icon} onClick={likePost} class={color}></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
+                        <ion-icon name={salvarPost} onClick={savePost}></ion-icon>
                     </div>
                 </div>
                 <div class="likeposts">
                     <img src="imglike/dog1.jpg" class="imglike" />
                     <p>
-                        Curtido por respondeai e outras 101.523 pessoas
+                        Curtido por respondeai e outras {props.numeroLike.toFixed(2) + like} pessoas
                     </p>
                 </div>
             </div>
@@ -34,16 +59,18 @@ function Post(props) {
 
 }
 
+
+
 export default function postMessage() {
 
     const posts = [
-        { imguser: "imgpost/gato5.jpg", paragrafo: " miar", imglike: "imglike/dog1.jpg" },
-        { imguser: "imgpost/dog3.jpg", paragrafo: " ouwn", imglike: "imgpost/gato3.jpg" },
-        { imguser: "imgpost/gato3.jpg", paragrafo: " miau", imglike: "imgpost/gato5.jpg"}
+        { imguser: "imgpost/gato5.jpg", paragrafo: " miar", imglike: "imglike/dog1.jpg", numeroLike: 101.520 },
+        { imguser: "imgpost/dog3.jpg", paragrafo: " ouwn", imglike: "imgpost/gato3.jpg",  numeroLike: 101.520},
+        { imguser: "imgpost/gato3.jpg", paragrafo: " miau", imglike: "imgpost/gato5.jpg",  numeroLike: 101.520 }
     ]
 
     return (
-        <div class="postagens">{posts.map((p) => (<Post imguser={p.imguser} paragrafo={p.paragrafo} imglike={p.imglike} />))}
+        <div class="postagens">{posts.map((p) => (<Post imguser={p.imguser} paragrafo={p.paragrafo} imglike={p.imglike} numeroLike={p.numeroLike}/>))}
         </div>
     )
 }
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,14 +1,32 @@
+import React from "react"
+
 export default function Usuario() {
+
+    const [usuario, setUsuario] = React.useState("escolhar um nome")
+    const [fotoUsuario, setFoto] = React.useState("https://www.frases.inf.br/wp-content/themes/webgo-content/assets/imagens/anonimo.png")
+
+
+    function username() {
+        let nomeUser = prompt("Qual é o seu nome?")
+        setUsuario(nomeUser)
+
+    }
+    function userFoto() {
+        let fotoUser = prompt("Qual o link da sua foto?")
+        setFoto(fotoUser)
+
+    }
+
     return (
         <div class="toposidebar">
-            <img src="/imgsidebar/gatoprofile.jpg" class="imgsidebar" />
+            <img src={fotoUsuario} class="imgsidebar" onClick={userFoto} />
             <div class="txtperfil">
                 <h3>
-                    catanacomics
+                    {usuario}
                 </h3>
                 <p>
-                    Catana
-                    <ion-icon name="pencil-outline"></ion-icon>
+                    {usuario}
+                    <ion-icon name="pencil-outline" onClick={username}></ion-icon>
                 </p>
             </div>
         </div>
